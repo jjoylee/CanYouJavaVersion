754d5de283b1f11ef07b6998737e35c49ca56599
jjoylee <ekdnlt1995@gmail.com>
2018-06-14 14:44:59 +0900

    session check and redirect method test and coding

--- src/test/java/com/canyou/util/LoginCheckInterceptorTest.java
+++ src/test/java/com/canyou/util/LoginCheckInterceptorTest.java
@@ -14,20 +14,47 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class LoginCheckInterceptorTest {
 	
+	LoginCheckInterceptor interceptor;
+	LoginCheckInterceptor spy;
+	Object handler;
+	HttpServletRequest request;
+	HttpServletResponse response;
+	
+	@Before
+	public void setUp(){
+		interceptor = new LoginCheckInterceptor();
+		spy = spy(interceptor);
+		handler = mock(Object.class);
+		request = mock(HttpServletRequest.class);
+		response = mock(HttpServletResponse.class);
+	}
+	
 	@Test
-	public void not_logged_in_test() throws IOException {
-		LoginCheckInterceptor interceptor = new LoginCheckInterceptor();
-		LoginCheckInterceptor spy = spy(interceptor);
-		Object handler = mock(Object.class);
-		HttpServletRequest request = mock(HttpServletRequest.class);
-		HttpServletResponse response = mock(HttpServletResponse.class);
+	public void redirectToLoginPage_test() throws IOException{
+		boolean result = interceptor.redirectToLoginPage(response);
+		assertFalse(result);
+		verify(response,times(1)).sendRedirect("/account/login");
+	}
+	
+	@Test
+	public void session_not_exist_test() throws IOException {
 		when(request.getSession(false)).thenReturn(null);
 		doReturn(false).when(spy).redirectToLoginPage(response);
-		boolean result = interceptor.preHandle(request, response, handler);
+		boolean result = spy.preHandle(request, response, handler);
 		verify(spy, times(1)).redirectToLoginPage(response);
+	}
+	
+	@Test
+	public void not_logged_in_test() throws IOException {
+//		HttpSession session = mock(HttpSession.class);
+//		when(request.getSession(false)).thenReturn(session);
+//		doReturn(false).when(spy).redirectToLoginPage(response);
+//		boolean result = spy.preHandle(request, response, handler);
+//		verify(spy, times(1)).redirectToLoginPage(response);
 	}
 }
