d6026b9c588cb7b2029daa135333b0e00e709652
jjoylee <ekdnlt1995@gmail.com>
2018-07-03 16:00:59 +0900

    create ResultService

--- src/main/java/com/canyou/service/Result/ResultService.java
+++ src/main/java/com/canyou/service/Result/ResultService.java
@@ -1,5 +1,30 @@
 package com.canyou.service.Result;
 
-public interface ResultService{
+import java.util.List;
 
+import com.canyou.model.LectureCategoryRequirement.LectureCategoryRequirementVO;
+import com.canyou.model.LectureDetail.LectureDetailVO;
+import com.canyou.model.LectureTypeRequirement.LectureTypeRequirementVO;
+import com.canyou.model.Result.ResultVO;
+import com.canyou.model.ScoreRequirement.ScoreRequirementVO;
+import com.canyou.model.Section.SectionVO;
+import com.canyou.model.SectionRequirement.SectionRequirementVO;
+
+public interface ResultService{
+	//Getter And Setter
+	List<LectureCategoryRequirementVO> getCategoryRequirements();
+	void setCategoryRequirements(List<LectureCategoryRequirementVO> categoryRequirements);
+	List<LectureTypeRequirementVO> getTypeRequirements();
+	void setTypeRequirements(List<LectureTypeRequirementVO> typeRequirements);
+	ScoreRequirementVO getScoreRequirement();
+	void setScoreRequirement(ScoreRequirementVO scoreRequirement);
+	SectionRequirementVO getSectionRequirement();
+	void setSectionRequirement(SectionRequirementVO sectionRequirement);
+	List<SectionVO> getSections();
+	void setSections(List<SectionVO> sections);
+	List<LectureDetailVO> getLectureDetails();
+	void setLectureDetails(List<LectureDetailVO> lectureDetails);
+	
+	//add Result
+	List<ResultVO> getResults();
 }
--- src/main/java/com/canyou/service/Result/ResultServiceImpl.java
+++ src/main/java/com/canyou/service/Result/ResultServiceImpl.java
@@ -1,5 +1,151 @@
 package com.canyou.service.Result;
 
-public class ResultServiceImpl implements ResultService{
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.canyou.model.LectureCategoryRequirement.LectureCategoryRequirementVO;
+import com.canyou.model.LectureDetail.LectureDetailVO;
+import com.canyou.model.LectureTypeRequirement.LectureTypeRequirementVO;
+import com.canyou.model.Result.ResultVO;
+import com.canyou.model.ScoreRequirement.ScoreRequirementVO;
+import com.canyou.model.Section.SectionVO;
+import com.canyou.model.SectionRequirement.SectionRequirementVO;
+
+@Service
+public class ResultServiceImpl implements ResultService{
+	
+	private List<LectureCategoryRequirementVO> categoryRequirements;
+	private List<LectureTypeRequirementVO> typeRequirements;
+	private ScoreRequirementVO scoreRequirement;
+	private SectionRequirementVO sectionRequirement;
+	private List<LectureDetailVO> lectureDetails;
+	private List<SectionVO> sections;
+	
+	public List<LectureCategoryRequirementVO> getCategoryRequirements() {
+		return categoryRequirements;
+	}
+	public void setCategoryRequirements(List<LectureCategoryRequirementVO> categoryRequirements) {
+		this.categoryRequirements = categoryRequirements;
+	}
+	public List<LectureTypeRequirementVO> getTypeRequirements() {
+		return typeRequirements;
+	}
+	public void setTypeRequirements(List<LectureTypeRequirementVO> typeRequirements) {
+		this.typeRequirements = typeRequirements;
+	}
+	public ScoreRequirementVO getScoreRequirement() {
+		return scoreRequirement;
+	}
+	public void setScoreRequirement(ScoreRequirementVO scoreRequirement) {
+		this.scoreRequirement = scoreRequirement;
+	}
+	public SectionRequirementVO getSectionRequirement() {
+		return sectionRequirement;
+	}
+	public void setSectionRequirement(SectionRequirementVO sectionRequirement) {
+		this.sectionRequirement = sectionRequirement;
+	}
+	public List<SectionVO> getSections() {
+		return sections;
+	}
+	public void setSections(List<SectionVO> sections) {
+		this.sections = sections;
+	}
+	public List<LectureDetailVO> getLectureDetails() {
+		return lectureDetails;
+	}
+	public void setLectureDetails(List<LectureDetailVO> lectureDetails) {
+		this.lectureDetails = lectureDetails;
+	}
+	
+	public List<ResultVO> getResults(){
+		List<ResultVO> results = new ArrayList<ResultVO>();
+		for(LectureCategoryRequirementVO requirement : categoryRequirements){
+			addCategoryRequirement(results, requirement);
+			addTypeRequirement(results, requirement);
+		}
+		addSectionRequirement(results);
+		addScoreRequirement(results);
+		return results;
+	}
+
+	private void addScoreRequirement(List<ResultVO> results) {
+		if(scoreRequirement == null) return;
+		ResultVO result = new ResultVO();
+		result.setName("총학점");
+		result.setRequirement(scoreRequirement.getCutline());
+		result.setScore(getTotalCredit());
+		results.add(result);
+	}
+	
+	private int getTotalCredit() {
+		int credit = 0;
+		for(LectureDetailVO detail : lectureDetails){
+			credit += detail.getCredit();
+		}
+		return credit;
+	}
+	
+	private void addSectionRequirement(List<ResultVO> results) {
+		if(sectionRequirement == null) return;
+		ResultVO result = new ResultVO();
+		result.setName("핵심 교양 이수 영역 수");
+		result.setRequirement(sectionRequirement.getCutline());
+		result.setScore(getCountBySection());
+		results.add(result);
+	}
+	
+	private int getCountBySection() {
+		List<Integer> sectionCount = new ArrayList<>();
+		for(LectureDetailVO detail : lectureDetails){
+			if(!isSection(detail.getSectionId())) continue;
+			if(sectionCount.contains(detail.getSectionId())) continue;
+			sectionCount.add(detail.getSectionId());
+		}
+		return sectionCount.size();
+	}
+	
+	private boolean isSection(int sectionId) {
+		for(SectionVO section : sections){
+			if(section.getId() == sectionId) return true;
+		}
+		return false;
+	}
+	private void addCategoryRequirement(List<ResultVO> results, LectureCategoryRequirementVO requirement) {
+		ResultVO result = new ResultVO();
+		result.setName(requirement.getLectureCategoryName());
+		result.setRequirement(requirement.getCutline());
+		result.setScore(getCreditByCategory(requirement.getLectureCategoryId()));
+		results.add(result);
+	}
+	
+	private void addTypeRequirement(List<ResultVO> results, LectureCategoryRequirementVO requirement) {
+		for(LectureTypeRequirementVO typeRequirement : typeRequirements){
+			if(typeRequirement.getLectureCategoryId() != requirement.getLectureCategoryId()) continue;
+			ResultVO result = new ResultVO();
+			result.setName(typeRequirement.getLectureTypeName() +" " + requirement.getLectureCategoryName());
+			result.setRequirement(typeRequirement.getCutline());
+			result.setScore(getCreditByType(typeRequirement.getLectureTypeId()));
+			results.add(result);
+		}
+	}
+	
+	private int getCreditByType(int lectureTypeId) {
+		int credit = 0;
+		for(LectureDetailVO detail : lectureDetails){
+			if(detail.getLectureTypeId() == lectureTypeId) 
+				credit += detail.getCredit();
+		}
+		return credit;
+	}
+	private int getCreditByCategory(int categoryId) {
+		int credit = 0;
+		for(LectureDetailVO detail : lectureDetails){
+			if(detail.getLectureCategoryId() == categoryId) 
+				credit += detail.getCredit();
+		}
+		return credit;
+	}
 }
